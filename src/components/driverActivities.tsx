7c5be7c4bc49dfeb1ba59d6d2bcadbef9cafd9bc
a.bilous <a.bilous@softum.io>
2024-07-24 12:59:39 +0200

    fix driver activities

--- src/components/driverActivities.tsx
+++ src/components/driverActivities.tsx
@@ -50,11 +50,20 @@ export default function DriverActivities({activities}) {
                 {activities.days.map((day, index) => {
                     const date = dayjs(day.date.date);
 
-                    const totalWork = day.activities.DRIVING;
+                    const totalDrive = day.activities.DRIVING;
                     const workDay = day.dayType != 'DAY_OFF';
 
                     let bgDay = 'bg-error/30';
                     let infoColor = 'fill-error';
+
+
+                    let driveStat = null;
+
+                    if(totalDrive >= 540){
+                        driveStat = '9+';
+                    }else if(totalDrive >= 480){
+                        driveStat = '8+';
+                    }
 
                     switch (day.dayType) {
                         case "FULL":
@@ -72,9 +81,15 @@ export default function DriverActivities({activities}) {
                     }
 
 
-
                     return <div key={day.date + index} className={`relative p-10 rounded-2xl shadow text-center ${bgDay}`}>
-                        {totalWork > 540 ? <div className="top-3 right-3 absolute"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className="w-5 h-5 fill-primary/80"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg></div> : ""}
+
+
+                        <div className="top-3 right-3 absolute">
+                            <div className="flex gap-2 items-center">
+                                {driveStat ? <span className="text-primary font-bold">{driveStat}</span> : ""}
+                                {day.nightDrive ? <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 fill-primary" viewBox="0 0 384 512"><path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/></svg> : ""}
+                            </div>
+                        </div>
 
                         {workDay ?
                             <div>
