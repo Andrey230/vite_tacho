35a4fc2b70cc56c7db2d01da267722a86f4d6d51
Andrey230 <andreybelous230@gmail.com>
2026-03-05 16:26:32 +0100

    add driver documents

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -2,10 +2,15 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../providers/AuthProvider";
 
 export default function RequireAuth() {
+
     const { isAuthenticated, loading } = useAuth();
 
     if (loading) {
-        return <div>Loading...</div>;
+        return (
+            <div className="min-h-screen flex items-center justify-center">
+                <span className="loading loading-spinner loading-lg text-primary"></span>
+            </div>
+        );
     }
 
     if (!isAuthenticated) {
